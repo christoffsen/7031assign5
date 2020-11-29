27bbd0ad858cebf8ea497cedcd79e41088a559b4
christoffsen <christopher.elder8@gmail.com>
2020-11-28 21:30:24 -0800

    Adding first part solution

---
+++ Assets/ImageCollection.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ImageCollection : MonoBehaviour
+{
+
+    GameObject [] images;
+    int baseZ = 1;
+
+    private int selectedIndex;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        selectedIndex = 0;
+        images = GameObject.FindGameObjectsWithTag("image");
+        Debug.Log("Images found: " + images.Length);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        for(int i = 0; i < images.Length; i++){
+            int newZ = baseZ + (i == selectedIndex ? 0 : 1);
+            images[i].transform.position = new Vector3(images[i].transform.position.x, images[i].transform.position.y, newZ);
+        }
+    }
+
+    public void setNext(){
+        if (selectedIndex == (images.Length - 1))
+        {
+            selectedIndex = 0;
+        }
+        else
+        {
+            selectedIndex += 1;
+        }
+    }
+
+    public void setPrevious(){
+        if(selectedIndex == 0)
+        {
+            selectedIndex = images.Length - 1;
+        }
+        else
+        { 
+            selectedIndex -= 1;
+        }
+    }
+}
---
+++ Assets/RightButtonClick.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RightButtonClick : MonoBehaviour
+{
+    ImageCollection images;
+    // Start is called before the first frame update
+    void Start()
+    {
+    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnMouseDown() {
+        Debug.Log("Right");
+        images.setNext();
+    }
+}
